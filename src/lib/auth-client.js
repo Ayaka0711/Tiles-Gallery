20da6ac3e9042490f830dc76c1f49b47a563018b
Ayaka0711 <minhaayaka399@gmail.com>
2026-05-05 19:54:18 +0600

    fix auth crash

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,8 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
-    
-    baseURL: "http://localhost:3000"
-})
-export const { signIn, signUp, useSession } = createAuthClient()
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL
+});
+
+
+export const { signIn, signUp, useSession } = authClient;
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,22 +3,22 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
+
+await client.connect();
+
 const db = client.db("tiles-gallery");
 
 export const auth = betterAuth({
-   emailAndPassword: { 
+  emailAndPassword: { 
     enabled: true, 
   }, 
   socialProviders:{
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret:process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }
   },
   database: mongodbAdapter(db, {
-
     client
   }),
 });
-
-
